bcfe482b1a6de8a3bcc94ec3a43b9a1219f08699
OfficialRaviJangra <insanejangra0001@gmail.com>
2025-08-25 09:18:20 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex justify-center">
+      <h1 className="text-xl font-sans">Online Voting App</h1>
+    </div>
+  );
+}
