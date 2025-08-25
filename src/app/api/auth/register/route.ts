5f969e4efc18bfe2d53446d66621e543b27f8096
OfficialRaviJangra <insanejangra0001@gmail.com>
2025-08-25 10:20:41 +0530

    Replace bcrypt with bcryptjs for password hashing and implement user registration API

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,55 @@
+import connectDB from "@/app/lib/db";
+import User from "@/app/models/User";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(request : NextRequest){
+    const {name , email, password, role} = await request.json();
+
+    try {
+        //connect to database
+        await connectDB();
+    
+        //check user exists in db or not 
+    
+        const existingUser = await User.findOne({
+            $or : [{name}, {email}]
+        })
+
+        if (existingUser) {
+            return NextResponse.json(
+                {error : "User already exists..."},
+                {status : 400}
+            )
+        }
+        
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create new user
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword,
+            role
+        });
+
+        // To Save the user
+
+        const savedUser = await newUser.save();
+
+        return NextResponse.json({
+            message: "User created successfully",
+            success: true,
+            savedUser
+        });
+
+    } catch (error) {
+        if (error instanceof Error) {
+            return NextResponse.json({error : error.message}, {status : 500})
+        }
+        return NextResponse.json({error : "Internal Server Error"},{status : 500})
+    }
+    
+}
