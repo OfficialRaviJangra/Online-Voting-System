21c54d1ccd1b808a770d7c51ec16dba0b808cf2a
OfficialRaviJangra <insanejangra0001@gmail.com>
2025-08-26 09:14:04 +0530

    Add login page with form handling and authentication logic

---
+++ src/app/login/page.tsx
@@ -0,0 +1,109 @@
+"use client"
+import axios from 'axios'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+const LoginPage = () => {
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
+    })
+    const [loading, setLoading] = useState(false)
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true)
+        try {
+            const response = await axios.post("/api/auth/login", user);
+
+            if (response.status === 200) {
+                router.push("/dashboard")
+            }
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                console.log(error)
+            }
+        } finally {
+            setLoading(false)
+        }
+
+    }
+    return (
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img
+                    alt="Your Company"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                    className="mx-auto h-10 w-auto"
+                />
+                <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                autoComplete="email"
+                                value={user.email}
+                                onChange={e => setUser({ ...user, email: e.target.value })}
+                                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
+                                Password
+                            </label>
+                            {/* <div className="text-sm">
+                                <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                                    Forgot password?
+                                </a>
+                            </div> */}
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                autoComplete="current-password"
+                                value={user.password}
+                                onChange={e => setUser({ ...user, password: e.target.value })}
+                                className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm/6 text-gray-400">
+                    Not a member?{' '}
+                    <Link href="/register" className="font-semibold text-indigo-400 hover:text-indigo-300">
+                        register now
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
