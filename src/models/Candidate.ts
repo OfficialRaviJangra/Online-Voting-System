0c511b835015d3bbbdb14c5faad888c4fff7d862
OfficialRaviJangra <insanejangra0001@gmail.com>
2025-08-26 09:44:22 +0530

    Add Candidate model

---
+++ src/models/Candidate.ts
@@ -0,0 +1,21 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface ICandidate extends Document {
+    name: string;
+    email: string;
+    party: string;
+    votes: Number;
+    manifesto: string;
+}
+
+const CandidateSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    party: { type: String, required: true },
+    votes: { type: Number, default: 0 },
+    manifesto: { type: String, required: true },
+}, { timestamps: true });
+
+const Candidate = mongoose.model<ICandidate>('Candidate', CandidateSchema);
+
+export default Candidate;
