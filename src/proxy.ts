a15d9de851305a21c5b244979b3862b06bd00c61
OfficialRaviJangra <insanejangra0001@gmail.com>
2026-02-18 11:02:18 +0530

    refactor: update image configuration and rename middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 import { verifyToken } from "./helpers/token.helper";
 
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const token = request.cookies.get("accessToken")?.value;
   const url = request.nextUrl.pathname;
 
